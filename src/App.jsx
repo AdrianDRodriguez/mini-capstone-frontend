8e9873d5c56f33cd5b59516e6c8b5c0205371da6
Adrian Rodriguez <ar9897.r@gmail.com>
2025-02-25 18:09:45 -0600

    Applying bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import { Header } from "./Header";
 import { ProductsPage } from "./ProductsPage";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 import { Footer } from "./Footer";
 
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <ProductsPage />
-      <Footer />
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -4,21 +4,22 @@ console.log(products);
   return (
     <div id="products-index">
       <h1>All products</h1>
-      <br />
-      {products.map((product) => (
-        <div key={product.id}>
-          <div>
-            Name: {product.name}
-          </div>
-          <div>
-            Price: {product.price}
-          </div>
-          <div>
-            <img src={product.image_url} />
-          </div>
-          <button onClick={() => onShow(product)}>More Info</button>
-        </div>
+      <div className="row">
+        {products.map(product => (
+          <div key={product.id} className="col-md-4">
+            <div className="card">
+              <img src={product.image_url} className="card-img-top" alt="..." height="200px" />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">{product.directions}.</p>
+                <button type="button" className="btn btn-primary" onClick={() => onShow(product)}>More Info</button>
+              </div>
+            </div>
+            </div>
       ))}
+      </div>
+    
+    
     </div>
   );
 }
